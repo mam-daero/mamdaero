d85c9a1a64af4337d95ff2be64a13c04bd140f09
박민준 <ppmm98@naver.com>
2024-07-24 14:58:36 +0900

    chore: camelCase, remove id
    Former-commit-id: 59d9508d7d4044f7ab8ac6a5d3b86e85f464ee2f

--- backend/src/main/java/com/example/global/entity/BaseEntity.java
+++ backend/src/main/java/com/example/global/entity/BaseEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7da472cf49119a28898564eecf69fee7238674ce6cc8d3c194116ad60ef2e661
-size 1033
+oid sha256:c78334e653b8679206914e2d3f60a8b69fec729ffc6dc230b90b7a2db5b7fc6e
+size 826
