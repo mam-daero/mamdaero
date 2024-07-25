962dd50f82272fa90169b6a303f2105280315458
허세령 <seryoii@naver.com>
2024-07-25 11:19:13 +0900

    fix: 버튼 안쓰는 변수 삭제 (#2)
    Former-commit-id: b50673083e2555eea06c25d6f4eb1e3890c4a7f3

--- frontend/src/components/button/RoundedButton.tsx
+++ frontend/src/components/button/RoundedButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd025a2eecc3a8bd1d10c1fbc7756707bea7e9a455384b4eaad563230fd4e02c
-size 687
+oid sha256:c2a4fb07c708eeb9ced3f78c82e6c80c14b96e18c2e7484629a4b7dfd6adc9ee
+size 638
