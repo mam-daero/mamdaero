509d4f24627b655c7f95ee6000892a505403b14e
박민준 <ppmm98@naver.com>
2024-07-25 13:05:08 +0900

    feat: counselor entity
    Former-commit-id: 739929ec1ee6b25f21a1d3f08803dc3960bce32d

---
+++ backend/src/main/java/com/example/mamdaero/member/entity/Counselor.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2af5c1ea717f62ec5b6438e61239555249ad5399e75375f3afefda7aaa7117d
+size 760
--- backend/src/main/java/com/example/mamdaero/member/entity/Member.java
+++ backend/src/main/java/com/example/mamdaero/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3129c1beea9e3fbd50f6f42419df6ce8f87f88b62f33a5acfa8efb0736bce45f
-size 1289
+oid sha256:1dc8a7707a0a5e42800753f3856149367267f74571442be8e2f20c17394a54ca
+size 1288
