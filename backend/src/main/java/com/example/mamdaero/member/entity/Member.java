96b8edc84df6073d5ab451e6976df758013e75f1
박민준 <ppmm98@naver.com>
2024-07-25 17:30:52 +0900

    feat: entity
    Former-commit-id: cf004d06eda5511d8497cdd3ab3e810a057269e0

--- backend/src/main/java/com/example/mamdaero/member/entity/Member.java
+++ backend/src/main/java/com/example/mamdaero/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0862e267789ef2fbe5739da8e348508224bc47983061681e6170cf59ea48d46
-size 1285
+oid sha256:89e63404b89a824f658cf3e0f95f4584c6bbeab881ac5c3ba607423da0d7262c
+size 1294
