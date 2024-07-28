0060df1dd45c57a44ec7749429d1dd06092c7338
박형준 <issg2030@gmail.com>
2024-07-28 16:17:50 +0900

    feat: 상당사 근무 상태 변경 (#9)
    Former-commit-id: 955154f769044748e32385cefa84ee7f09ab175b

--- backend/src/main/java/com/mamdaero/domain/work_schedule/service/WorkTimeService.java
+++ backend/src/main/java/com/mamdaero/domain/work_schedule/service/WorkTimeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99c7e93168fc2001c6bb4bf54779af7b960f8abcb6a19d4991406683c33a6197
-size 2992
+oid sha256:b64e0bccff4bce1fc1b4d957e19ddd4b1b9abf68cb99204e55cd22ee5ea38ca2
+size 3478
