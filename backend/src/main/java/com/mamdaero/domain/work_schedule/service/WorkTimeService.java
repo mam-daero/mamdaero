2c9eae911b8546fd78b92aa0899ebb9ddbccb651
박형준 <issg2030@gmail.com>
2024-07-28 15:48:44 +0900

    feat: 상담사 최초 등록 시 근무시간 생성 기능 (#9)
    Former-commit-id: bcab7224058f5a11bff8da3f44533b471f02acb4

--- backend/src/main/java/com/mamdaero/domain/work_schedule/service/WorkTimeService.java
+++ backend/src/main/java/com/mamdaero/domain/work_schedule/service/WorkTimeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4d0ad2f9bef9ef692d9ac31805e8f65e4a59f61c79020fa5f8215759f3c36b4
-size 2223
+oid sha256:ff77f004fac22a398f723f74ed97d5a767db9a37e38516a586a9f83cd6349d87
+size 2992
