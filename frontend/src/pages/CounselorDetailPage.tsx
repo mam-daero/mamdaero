5127e11dc7f2f730c93da0cafbdc3d62ba64a8f3
허세령 <seryoii@naver.com>
2024-07-30 20:36:00 +0900

    fix: 상담사 상세 페이지 route 수정 (#24)
    Former-commit-id: 9b470e131919216e44fd6d27b29d72e23926997b

--- frontend/src/pages/CounselorDetailPage.tsx
+++ frontend/src/pages/CounselorDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2a953095fa02bb26d5030c728eefab2399a03bb43cdf11e3ca4de7f27a80808
-size 4058
+oid sha256:e9ea98e5057ae3f6463e063e7640850937a8f82a246a54cb79252c9e76987054
+size 4834
