2620959b96db863f6305283edc87daf502ca6eeb
허세령 <seryoii@naver.com>
2024-07-30 13:34:31 +0900

    add: 상담사 급수 정렬 dropdown 추가 (#14)
    Former-commit-id: 73b0ad9bace950a26e919c5631664eb6d51a5298

--- frontend/src/components/input/SignUpCounselorInfo.tsx
+++ frontend/src/components/input/SignUpCounselorInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a97146683285444508b2e3901077825dd628fc06367f567f8c60c0d230cc20ea
+oid sha256:0e8cd5d1bb80f312530cabb4eee8afb65b3c67bbd86106e952639d45474f1b46
 size 5944
