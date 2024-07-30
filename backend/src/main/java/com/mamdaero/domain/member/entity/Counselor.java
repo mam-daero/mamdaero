2e596a3bb6e9aebbc70f182b087fda6280dc3977
박민준 <ppmm98@naver.com>
2024-07-30 16:00:27 +0900

    fix: mergeFix
    Former-commit-id: 36e784e84458994efdd0ddd3968004d68e93067e

--- backend/src/main/java/com/mamdaero/domain/member/entity/Counselor.java
+++ backend/src/main/java/com/mamdaero/domain/member/entity/Counselor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53ef4bcf4c9fb69b449682a94f5746c2d080a8b3d574c9bfcab9d264cabc5cf6
-size 1269
+oid sha256:9d2b4afb1aadda251081581aec69b9c4a083ebdacb2f14ce93e3e1340f414a5e
+size 1190
