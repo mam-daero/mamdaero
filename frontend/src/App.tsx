6871bd9ed8af48a777a6a2b9f584737e10d5b2ac
허세령 <seryoii@naver.com>
2024-07-30 20:14:01 +0900

    feat: 상담사 정보 상세 페이지 및 리뷰 컴포넌트 추가 (#24)
    Former-commit-id: f1b20da0ac7b92a65b6740e5a58448c1f49668af

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8926e568c71bbde8c21634cc02d510f9e0584aca7f0c6e5e1e8f9a94cb39f0c
-size 1618
+oid sha256:6a21700a4be9e36ae0cec320afe874153a6dd2f757e156ba7eea976f92ed10fc
+size 1765
---
+++ frontend/src/components/card/ReviewCard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2861a96517d76d4013b6a7f424223ba6b12694b089d5320b4d7e536a7c8e5632
+size 586
