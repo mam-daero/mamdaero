966310597f97a7acf574e174e357f58164e985b5
박민준 <ppmm98@naver.com>
2024-07-30 15:10:30 +0900

    feat: Report update
    Former-commit-id: a3c93422227558896a79d8e76daac6d842ceb0b9

--- backend/src/main/java/com/mamdaero/domain/consulting_report/entity/ConsultingReport.java
+++ backend/src/main/java/com/mamdaero/domain/consulting_report/entity/ConsultingReport.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:296373f68f7f852dd84c98b26d0c86e8e992e537c97a60ce6db29b55b4606943
-size 846
+oid sha256:097186bbf42cead904dd8f94795706550b16ac905b0b8f2f68f433d9dc38619e
+size 1098
