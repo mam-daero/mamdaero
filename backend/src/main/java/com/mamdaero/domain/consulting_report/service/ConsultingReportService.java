5431c389e7b8749543cb61d08aac037938856f35
박민준 <ppmm98@naver.com>
2024-07-31 16:40:08 +0900

    add: serial annotation
    Former-commit-id: 8a188c8ebe1657f787a79912e345c130036174ec

--- backend/src/main/java/com/mamdaero/domain/consulting_report/service/ConsultingReportService.java
+++ backend/src/main/java/com/mamdaero/domain/consulting_report/service/ConsultingReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f0d4a01a6dd66e9ab482799acfead30e80bc8956f5d7e5f08e85a144a3d9b9d
-size 3551
+oid sha256:407902c6fadcf554f3cb96a28a943a6a63f11205f3b5bb99abe1534f065f240b
+size 3542
