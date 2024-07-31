9c3be6a5bccb5e12165fa54b784d1bd7e569e8ba
박민준 <ppmm98@naver.com>
2024-07-31 16:40:49 +0900

    add: serial annotation
    Former-commit-id: 35169fbdd559d781bc357b596c2155503c678f38

--- backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1880eb7aaf2dc9cefe0e3ea53e5b697a20ecfbe587c73ca110d5231a5e785b0d
-size 2898
+oid sha256:07721cf3c686f7ca38b0f9e496abd680305ab7eae86bc82db085266cd625165f
+size 3033
