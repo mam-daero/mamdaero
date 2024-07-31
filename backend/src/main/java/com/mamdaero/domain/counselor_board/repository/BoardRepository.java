fb5af2720354420121648e1f8b3be077cc1efe84
신혜민 <alddlv81@gmail.com>
2024-07-31 16:51:23 +0900

    feat: 게시글 좋아요 수, 나의 게시글, 나의 좋아요 여부 추가 (#15)
    Former-commit-id: 4e803e7344bfaeca971292eed475a636dcd3c51d

--- backend/src/main/java/com/mamdaero/domain/counselor_board/repository/BoardRepository.java
+++ backend/src/main/java/com/mamdaero/domain/counselor_board/repository/BoardRepository.java
@@ -4,4 +4,5 @@ import com.mamdaero.domain.counselor_board.entity.CounselorBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<CounselorBoard, Long> {
+    boolean existsByIdAndMemberId(Long id, Long memberId);
 }
