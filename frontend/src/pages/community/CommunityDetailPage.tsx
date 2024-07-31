eae5b5b8324974009c2b39040f9f9158568be0ed
허세령 <seryoii@naver.com>
2024-08-01 05:27:43 +0900

    feat: 상담사 슈퍼비전 관련 페이지, 컴포넌트 추가 (#29)
    Former-commit-id: 6903f5a3c9d1d590942d38907af8999ef7741afb

--- frontend/src/pages/community/CommunityDetailPage.tsx
+++ frontend/src/pages/community/CommunityDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9a7e2fb6bbf1dc77fd2ac5c6075141c081752fd23d5b03bc534938861df0121
-size 2003
+oid sha256:ee4b1c6278c6a2f9962305db74c284a764db824007be47de41e8c377ade0079b
+size 1938
