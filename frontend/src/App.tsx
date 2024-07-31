fed33681aeb3c299b627336f8bfd7fca25a3bb58
허세령 <seryoii@naver.com>
2024-07-31 10:22:13 +0900

    feat: 게시판 목록 테이블 컴포넌트, 커뮤니티 페이지 추가 (#25)
    Former-commit-id: bcd4d6b18ebb68c19714fefd3cd018e7d7a65e5c

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b03e3212bae7c669133c53e3a65dd74b8e57abcc41aabc2c0a898ba1fb92881
-size 1926
+oid sha256:ed9eeebfc178098c947b7d07c2451f60815d0982561368160887319f753d519a
+size 2054
