8094bffae18a078c77ba49ee223133327f48dab9
이재빈 <jeabin1129@naver.com>
2024-08-01 10:42:55 +0900

    fix: myPage 에러코드 수정 (#26)
    Former-commit-id: 6bb9b8284d37cbb186ed0b92da5fb080de1158e1

--- frontend/src/pages/mypage/ClientMyPage.tsx
+++ frontend/src/pages/mypage/ClientMyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b3837a8dd2c580345209d237d4be1b6ec237526d5fc36811bb76805b15f4cda
-size 4096
+oid sha256:e6e34fd0b2c5ab4961a7d2a97f9e8748feeeef014f8c8d178f53c0d3e49ab290
+size 4179
--- frontend/src/pages/mypage/CounselorMyPage.tsx
+++ frontend/src/pages/mypage/CounselorMyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc22bc6b29807fdbc9ae9271c743918f608c60534df0e337445a4e52a69f499e
-size 5260
+oid sha256:8ae257e69f60a81ae2f7681b502b96f8583e42b144f016253fe77d0623043ad4
+size 5356
