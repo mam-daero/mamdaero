04cd82cd156efe5ce3e08288897820e216345a07
신혜민 <alddlv81@gmail.com>
2024-08-01 13:25:05 +0900

    feat 공지사항 검색 기능 추가 (#11)
    Former-commit-id: d9c96cca3edadee1c8dd62ab61f142867e73d7e0

--- backend/src/main/java/com/mamdaero/domain/notice/service/PNoticeService.java
+++ backend/src/main/java/com/mamdaero/domain/notice/service/PNoticeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b211b975f84da69a7faac4589272ce562c24ded2520c0ac8fe20d7c814a22930
-size 1208
+oid sha256:26b4cb98f24b2729d33dc9a0195d59b3a2ec7a9b7b358b3f39719a0dd2f158d8
+size 2175
