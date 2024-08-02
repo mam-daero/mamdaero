2033611946d3d96babff08c416ddb56b44d211b7
허세령 <seryoii@naver.com>
2024-08-02 10:44:38 +0900

    feat: 댓글 작성 카드 컴포넌트 추가 (#33)
    Former-commit-id: 8042276be1a5fe9f474e0f1b7067d93b3174c7c9

--- frontend/src/components/card/community/CommunityWriteCommentCard.tsx
+++ frontend/src/components/card/community/CommunityWriteCommentCard.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-const CommunityWriteCommentCard = () => {
-  return (
-    <div>
-      <input type="text" />
-    </div>
-  );
-};
-
-export default CommunityWriteCommentCard;
+version https://git-lfs.github.com/spec/v1
+oid sha256:3239278cfff45cda4623a1dfdb466392e488b1c9f820a859cfb3bb66396464fe
+size 792
