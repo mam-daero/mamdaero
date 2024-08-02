fe3736a3f95063df8f4a6ab74d5cce26537c899d
신혜민 <alddlv81@gmail.com>
2024-08-02 15:51:14 +0900

    feat: 포스트잇 질문 추가, 조회(현재는 하루마다 변경) (#22)
    Former-commit-id: ffe5b63763bee1999f7b0a10c2f9224169d79d73

---
+++ backend/src/main/java/com/mamdaero/domain/question/controller/QuestionController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:059c49e06e4808c8235800febdf642c30cf6bca577c99cafaa900e9052c83bf4
+size 996
---
+++ backend/src/main/java/com/mamdaero/domain/question/dto/response/QuestionResponse.java
@@ -0,0 +1,12 @@
+package com.mamdaero.domain.question.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QuestionResponse {
+
+    private Long id;
+    private String content;
+}
