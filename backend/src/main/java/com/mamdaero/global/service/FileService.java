f4188374522847e9354f53922f2ccbb45ffdadad
신혜민 <alddlv81@gmail.com>
2024-08-02 09:35:12 +0900

    fix: 사진 없을 때 저장 안되게 수정 (#34)
    Former-commit-id: d852a8de46c31a44197002e85cc0ff7655dff782

--- backend/src/main/java/com/mamdaero/global/service/FileService.java
+++ backend/src/main/java/com/mamdaero/global/service/FileService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84448a7399addf5b5fff5a4ba5bfd89ee74ab5fa5331f8ecff010447830e2067
-size 2103
+oid sha256:a49cdbc3c02ad9906d9b8d0f1842e3f43b8b6daf90713f214565b3b45cee13ce
+size 2141
