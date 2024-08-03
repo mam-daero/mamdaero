45ad86e6e50345e857be160b572c0fe03e3e1913
박형준 <issg2030@gmail.com>
2024-08-03 21:39:55 +0900

    feat: 존재하지 않는 근무시간 예외클래스 추가 (#9)
    Former-commit-id: abed0bd81903da1be67202b1adc02c1c240799e9

--- backend/src/main/java/com/mamdaero/domain/work_schedule/exception/WorkScheduleExceptionConstants.java
+++ backend/src/main/java/com/mamdaero/domain/work_schedule/exception/WorkScheduleExceptionConstants.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59db801613f507adf8bce8c72f327015632c989a9cd625dce2bd5a07ca3e6d1e
-size 698
+oid sha256:b8094834764528374d77c2737ef8ca1c010cbc7c05823b0fdda71e2f9a32c5f6
+size 755
