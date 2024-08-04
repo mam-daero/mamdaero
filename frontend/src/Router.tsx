2f30c5dd2fe5fc5770468ce6f6ba83c8bbdd68cb
박주영 <pjy7244@gmail.com>
2024-08-04 14:52:35 +0900

    add: 다음에 만들 나의상담-상담자 페이지 라우터 추가 (#39)
    Former-commit-id: 37b0d5c82b3059bb758a3d83e4d2cca5dcc60c0f

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e95fd608c57d56d7a7881a1b7b6c353779fbcae973b350633a8856e8d84733
-size 4794
+oid sha256:c33c26cfed9dd2260ec11438be3c708b34495394aeeb6bab12223ce1c9bd51a5
+size 5315
---
+++ frontend/src/pages/mycounsel/counselor/CounselRecordList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CounselRecordList: React.FC = () => {
+  return (
+    <div>
+      <div>작성중</div>
+    </div>
+  );
+};
+
+export default CounselRecordList;
