ef9aedae9296b4303254fa05cf2698a9b9eac983
신혜민 <alddlv81@gmail.com>
2024-08-05 13:54:48 +0900

    fix: repository 오류 수정
    Former-commit-id: 4228daec76e6a56e72820c5efd3998ac14e6e95c

--- backend/src/main/java/com/mamdaero/domain/reservation/service/ReservationService.java
+++ backend/src/main/java/com/mamdaero/domain/reservation/service/ReservationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a13a62d29209c782055600fe374bddfaecec68073375ca47955c9c6c222d3e32
-size 7534
+oid sha256:88b01b1ec85c34515aa1ec0318db986cb2dfbb7749086ab9d6b3de56b696cada
+size 7523
