368074b85aebe97132a437944c8290453f8c448b
신혜민 <alddlv81@gmail.com>
2024-08-06 16:18:59 +0900

    refactor: 파일 확장자 빠진거 설정 (#34)
    Former-commit-id: 1fb07fb991f3c099b996bd8040a5f792a6cf8031

--- backend/src/main/java/com/mamdaero/global/service/FileService.java
+++ backend/src/main/java/com/mamdaero/global/service/FileService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7a481ba8592b0572fab912e36c5d4244b7dd76dad351b8a86667bb96be76f85
-size 2722
+oid sha256:4fabe9f16054123e76e17314a607f32b921fee25e8f710a56c82acf0134f5b57
+size 2767
