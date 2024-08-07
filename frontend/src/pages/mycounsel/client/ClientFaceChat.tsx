73f35a4c20153879a0d61e7e4da2bf11cc75f69f
박주영 <pjy7244@gmail.com>
2024-08-08 03:12:05 +0900

    add: 상단바 통일, 화상채팅 페이지 만들기(#40)
    Former-commit-id: dd7d4709c059b3c966d795353fa631ccb947c6dc

--- frontend/src/pages/mycounsel/client/ClientFaceChat.tsx
+++ frontend/src/pages/mycounsel/client/ClientFaceChat.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-const ClientFaceChat: React.FC = () => {
-  return <div>작성중</div>;
-};
-
-export default ClientFaceChat;
+version https://git-lfs.github.com/spec/v1
+oid sha256:09e10a0c0c5b14d1d11add29d0ecde9e5cef5a4927cc43ee8bdd1d82e0871a9d
+size 15633
--- frontend/src/pages/mycounsel/counselor/CounselHistory.tsx
+++ frontend/src/pages/mycounsel/counselor/CounselHistory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d0a19b7f769adf577e3225d47227c3451b872aadd6ed4ad318fd58184a1e15e
-size 3409
+oid sha256:63b8027681b81bbb24ec5826a96ccd34a2021f1556d814af78953aa2ba094695
+size 3717
