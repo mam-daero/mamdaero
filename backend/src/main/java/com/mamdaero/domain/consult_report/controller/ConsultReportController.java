9fa6da7e9ecf0df8a6977da294b78aa784f88889
박형준 <issg2030@gmail.com>
2024-08-07 09:34:09 +0900

    fix: controller mapping url 오타 수정 (#47)
    Former-commit-id: f015a76153542af04f21e2821f837e2d7ec58326

--- backend/src/main/java/com/mamdaero/domain/consult_report/controller/ConsultReportController.java
+++ backend/src/main/java/com/mamdaero/domain/consult_report/controller/ConsultReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5247e19f95969d898930e568b73d5a2df94482e027f2616e60e2f38da503cfb
-size 1338
+oid sha256:637b9465db77c0c8b830acb1d0d8117734d73588e01d6cdd7c861d33edfbef66
+size 1345
