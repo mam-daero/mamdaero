d7d223338de2cf649fc57bac56e38ba9212deab9
박민준 <ppmm98@naver.com>
2024-08-08 17:32:36 +0900

    refactor: Add Auth Role
    Former-commit-id: 4a093241b6cfd45576d2e7804659b152f6a5fea5

--- backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
+++ backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:460ac497db14b8e872e7f689bcc2921879ea20158cae8d8cecfcf2ef683d7606
-size 3064
+oid sha256:0a6c8892a60d3238b9a1f20393ff33c4736ccb6579a9c0f9cf53a2c4876c9205
+size 3725
