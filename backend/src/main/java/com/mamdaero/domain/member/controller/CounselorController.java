fd18c7364f5359ced607f0ca2f318c502c748d86
박민준 <ppmm98@naver.com>
2024-08-08 16:42:23 +0900

    fix: url
    Former-commit-id: 7ee746f82fb0784fe44ed07a9e61dabb6c1f09b1

--- backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
+++ backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a0a43409b3c65be1535b8f70ee18296922349c045a8ee7f7f3b5e9715a16d3
-size 3054
+oid sha256:460ac497db14b8e872e7f689bcc2921879ea20158cae8d8cecfcf2ef683d7606
+size 3064
--- backend/src/main/java/com/mamdaero/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/mamdaero/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc2b6345ef1169bbd02f692e7a8b109ddbc81954260526f15fd94d3768f5afd9
-size 1395
+oid sha256:e709fae1bde75b526b2871ad3794b5631a45744e00bf017e3fa54a57c25a09bf
+size 1399
