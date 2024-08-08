0ec6343c2e1dedb03d8a312e90282e0ef8672fc2
허세령 <seryoii@naver.com>
2024-08-08 12:57:53 +0900

    add: 댓글 수정, 삭제 기능 (#37)
    Former-commit-id: 9a650f3eaefcb17613e38ffd1da4ae65c3e38055

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ac19a5f2e13d1a2cfa525a8a36fe13513c10200622a5bb4f47f9cd3bcfc0233
-size 1739
+oid sha256:ff1004d81fe124eaf238c0ee2865cb66af7c6812ae96375eaaaa568a00d06293
+size 1797
--- frontend/src/components/card/supervision/SupervisionPostCard.tsx
+++ frontend/src/components/card/supervision/SupervisionPostCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9d62728f8abc3608d828b8188fa259fcc99b633b1ce8b28792540fbbe9c2907
-size 5063
+oid sha256:6957df900745a7488d736d41adf919a479fb90db70de1dcd0382848b5aaf8748
+size 5064
