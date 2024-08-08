8aefaf6233671b52eb5d0e5b8f73ae6522d1cecb
brobac <issg2030@gmail.com>
2024-08-08 22:47:54 +0900

    feat: 리뷰 수정 - 권한, 멤버 로직 추가 (#61)
    Former-commit-id: 731c2dd29494181ecc55269e53d9e977bb793fbe

--- backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf4f8bf87d17ba90173e1f29ab285d3c62e408be36c35addb16f3aacb2944217
-size 4975
+oid sha256:6059d3d23827834f8fbf8d27e0f1482ff9b22c7d8bea4719da7af42769fc3a7c
+size 5087
