d44758165faa9c0d9966558e750db8552ede7546
박민준 <ppmm98@naver.com>
2024-08-08 15:40:10 +0900

    refactor: Diary With Token Auth
    Former-commit-id: 6afaa5772ad9659a78d4cba6b37d075aae926782

--- backend/src/main/java/com/mamdaero/domain/diary/service/DiaryService.java
+++ backend/src/main/java/com/mamdaero/domain/diary/service/DiaryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:053a7d496409f9e825eda1b88492bd68c4f246c49eb34c2381fbb02039db87fa
-size 4596
+oid sha256:35b23e85e31b27da8ffbf89473319a3409e7b85a749660a61d17eeb71bb96979
+size 4648
