24699d85e299c1544d1517f45e2da5c4501f790b
brobac <issg2030@gmail.com>
2024-08-08 22:57:22 +0900

    feat: 보고서 상세조회 - 권한, 멤버 로직 추가 (#62)
    Former-commit-id: eadfe0b62f56121282e1573dd72eba86dd8c0085

--- backend/src/main/java/com/mamdaero/domain/consult_report/service/ConsultReportService.java
+++ backend/src/main/java/com/mamdaero/domain/consult_report/service/ConsultReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aabdd228c02c83ee67c71025d120d8d5ab11510459de21eea573282c731a67d1
-size 5556
+oid sha256:8b72f9266be73e234cb27202cd7ebfb8604495ee77dd0e9743a475b182469273
+size 5705
