d226da91b25166d066f171cf66dfcf3a5104690d
신혜민 <alddlv81@gmail.com>
2024-08-08 17:24:42 +0900

    feat: 자유 게시판 알림 (#59)
    Former-commit-id: bfcb037027b184667c2e5c909fa336cd50edda63

--- backend/src/main/java/com/mamdaero/domain/board/controller/BoardCommentController.java
+++ backend/src/main/java/com/mamdaero/domain/board/controller/BoardCommentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f72193f6d9ba242f6c575af2245601ddfaba37d83554d1eff005b73efc8d0180
-size 2302
+oid sha256:d460a3e43b7e9fa4877c00e1acdeccaea6321bed8a032e8c44d3551bcdf7637d
+size 2294
