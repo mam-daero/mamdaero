eda3d1f216ea32e49d5ddd978f13254f0f2216e4
박민준 <ppmm98@naver.com>
2024-08-08 15:13:34 +0900

    refactor: Counselor With Token Auth
    Former-commit-id: 5913b2b1e133808ce86f82ed3c599a45d63ef54d

--- backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
+++ backend/src/main/java/com/mamdaero/domain/member/controller/CounselorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7970d71643494f5af8cab84df4d0c9c87346de688c3602f61de98077fd830fd9
-size 2759
+oid sha256:e8a0a43409b3c65be1535b8f70ee18296922349c045a8ee7f7f3b5e9715a16d3
+size 3054
