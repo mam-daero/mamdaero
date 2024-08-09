0fe099798bebb0ddfe77a2d8c168d61fb4478008
박민준 <ppmm98@naver.com>
2024-08-09 16:47:35 +0900

    feat: webRTC
    Former-commit-id: 50ef47985c73b20fd534e954ef24c08dba5e10e7

--- backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
+++ backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec001f4325170927f04d61fa4e321781b687617e5d74970dab25b5722d33398
-size 956
+oid sha256:4fef0dcf4039a2914583fa2bbd28c9c21278c08fc6dfa03eddcd8d6d99cb1966
+size 744
