55bfbe98df3ed5dc89803e0f58c867bcba2804e2
신혜민 <alddlv81@gmail.com>
2024-08-09 16:33:50 +0900

    fix: 서버 오류 수정
    Former-commit-id: e0fb7a48b63026212339d5897c4335f29ba7b7d3

--- backend/src/main/java/com/mamdaero/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/mamdaero/domain/member/repository/MemberRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cb3ef4402ee7f1a6541aafc0f0347e32947f539535e80d994ff33555a4c3e67
-size 916
+oid sha256:d96bcd08aaa0bca37a541a5390caf1e0aaf0ec757cec0d8c9dcd82aed234e902
+size 874
