097d7732eac7a1916a3bf8a5e94c45eb992cb630
박민준 <ppmm98@naver.com>
2024-08-11 03:06:19 +0900

    refactor: webRTC
    Former-commit-id: 8312f5537f9ed4f2a6a1ba10a4a4833c7f720a93

--- backend/src/main/java/com/mamdaero/domain/member/security/config/SecurityConfig.java
+++ backend/src/main/java/com/mamdaero/domain/member/security/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4c47cfb49f8864a4fe2300977bae64fac1889110bb7a4dec25e313960f307dc
-size 5995
+oid sha256:b478802cae9048051bf4321c28bb45d04a0f72e203131ab9db7d59f2e9a99ec2
+size 6017
