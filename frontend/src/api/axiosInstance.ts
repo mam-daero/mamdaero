8cb6f90ce856333ce8c2707143a841224fc84a2c
허세령 <seryoii@naver.com>
2024-08-11 01:43:43 +0900

    Update axiosInstance.ts
    Former-commit-id: e9de8c413690e9b61077a96f514d84b57e67c309

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46dba7b6ec54cb79b74a256d843c881484ce9af7f43629f33ef0b1181f6a4330
-size 712
+oid sha256:24e28714e24a6c899c9952270b2f6f3df627bea81912feb6d7e56c8e846c359e
+size 635
