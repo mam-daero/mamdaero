6d0e2d35a293592c1657636ccdb2c7eaba6a9750
박민준 <ppmm98@naver.com>
2024-08-10 20:47:40 +0900

    refactor: diary
    Former-commit-id: 816f578d3f561e2be8020fe343fab96a2d642f1f

--- backend/src/main/java/com/mamdaero/domain/diary/repository/DiaryRepository.java
+++ backend/src/main/java/com/mamdaero/domain/diary/repository/DiaryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a62f7d5d656ba0cc8ba45a957b55a8c5d1971e9f5ec0c07002f4d8a7fc5012
-size 1643
+oid sha256:6fc9836a053ca8295065a16573c7ddc488e0e62bf0b0378155620b4ed8082de9
+size 1660
