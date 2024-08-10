069744136ec2d9c1d69a40ae3e336c0289e9e635
박주영 <pjy7244@gmail.com>
2024-08-11 00:36:00 +0900

    add: 마이페이지 수정 (#26)
    Former-commit-id: 3fc4d6bad642d42dc6e077b41d3dac3e16a66081

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18b3605f8830128f375ec9455b76019bc84eca7b73a59a894cbaecb54e694382
-size 1536
+oid sha256:b67ea65dee64b9c6ba7d1fcea5804acb3debac457499c69e134c3dd9f06068e9
+size 1705
--- frontend/src/pages/mypage/CounselorMyPage.tsx
+++ frontend/src/pages/mypage/CounselorMyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fd33bc74681ebb873f5793a5a676c6ce8c205614ca472d1d879d37d267b68c1
-size 7424
+oid sha256:440146f5e6bc0558144e96c7a9432a00faf797c9ded156f77e6edc602bf09149
+size 12522
