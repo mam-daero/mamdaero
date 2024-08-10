644c3d501a77c14dddc7a8a0dacc569045db40e3
이재빈 <jeabin1129@naver.com>
2024-08-11 01:09:40 +0900

    Before Merging
    Former-commit-id: 194cef9924e3d8431cc4fe425343159f485efc90

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c068d5aae6ffce9029ffb7a3c77f328fdc5bd1fe3cad7cfac75556a1f297c6ef
-size 10067
+oid sha256:aabad8e4d03073565e513fc80105fa14a994ac459f5e19066d4cd9e75e618d61
+size 10075
