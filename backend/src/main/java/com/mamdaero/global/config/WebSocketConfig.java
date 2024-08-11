90d9f1cba4a061767a7cdfde1344408d40c63c78
박민준 <ppmm98@naver.com>
2024-08-11 16:09:46 +0900

    refactor: WebRTC
    Former-commit-id: 24e8f1ab4a3b79edd9ddb2ceea712e971053fc35

--- backend/src/main/java/com/mamdaero/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/mamdaero/global/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddab40b1ef23c20022bbad717504326e7f4911475bdf04afb31ee03a6a414b94
-size 1673
+oid sha256:022c6ba0b354a30d7df274d73c87699afcaf4f920269149ede5dabd2105a648b
+size 3239
