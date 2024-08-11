451441547fc282f431b2d8cdbfdb5b3b3784349f
박민준 <ppmm98@naver.com>
2024-08-11 18:10:48 +0900

    refactor: Postit
    Former-commit-id: 18b82bdb007735d3d10af3c65d38107641bbbd19

--- backend/src/main/java/com/mamdaero/domain/postit/controller/PostitController.java
+++ backend/src/main/java/com/mamdaero/domain/postit/controller/PostitController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b06df8f796ed2814bb64279334527a5a002a85f443766db660eab86595583d0d
-size 3109
+oid sha256:da8c0d1f9993fd8e7f77cbe6cb24197bdf0c9f43136540f9fec26c88084e747e
+size 3064
--- backend/src/main/java/com/mamdaero/domain/postit/service/PostitService.java
+++ backend/src/main/java/com/mamdaero/domain/postit/service/PostitService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08a57acb0ae8f9754f110d3581d33479772071ab7347b6ef8eceb032917a0a69
-size 4697
+oid sha256:2e025baa7501599be223a723b3b294f2bdf160f1aab10a401f002e92d95ea28c
+size 4674
