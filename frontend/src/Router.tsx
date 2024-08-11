1ee15954a67eb0363a2ade9e532b31a6f1885945
박주영 <pjy7244@gmail.com>
2024-08-12 01:57:19 +0900

    add: 마이페이지 프로필수정 API연결 (#77)
    Former-commit-id: 7f8c252aacb87604d8da383a9a446ca41ca44337

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5df132627fa3550f2c8de797f5d0b6962894b836c6f09b291d0f5c7c6de77f3d
-size 7680
+oid sha256:82fcfa2de730f0b1acf4f88827e10bf06234cee4128228b137617ae3ec231c7a
+size 8002
--- frontend/src/stores/memberStore.tsx
+++ frontend/src/stores/memberStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3c7860da062b4ae46a258c8b6f40b882eff3ba15c65bac60495cf9a3299cb6a
-size 1625
+oid sha256:ec14bb512bf3622d236696a1a50004a5d9f92452ce5a183d67bf36d568478eb0
+size 2926
