f77c0a0d3daa280a75d42b9e6f36b30723165952
박민준 <ppmm98@naver.com>
2024-08-12 11:12:39 +0900

    refactor: Counselor Review Count, Rate
    Former-commit-id: ba5570cd4aafde4f0091867e8836cd8545153e49

--- backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
+++ backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fef0dcf4039a2914583fa2bbd28c9c21278c08fc6dfa03eddcd8d6d99cb1966
-size 744
+oid sha256:3b7fe7543a93364fb8050438a149261ec1f3065c1d08ed6cb4b68c10eaac7c93
+size 2617
