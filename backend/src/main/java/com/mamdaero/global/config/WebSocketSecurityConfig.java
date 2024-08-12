f62369d3c82dc418b70d854e593fc6ba77ed08b5
박민준 <ppmm98@naver.com>
2024-08-12 15:54:45 +0900

    refactor: WebRTC error
    Former-commit-id: b5f98d5f8721f839bb8c1efa99d2a792f8ab1de7

--- backend/src/main/java/com/mamdaero/global/config/WebSocketSecurityConfig.java
+++ backend/src/main/java/com/mamdaero/global/config/WebSocketSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cdf14ad86c177358a66fe97a3246cec539f08dc755a52704e27671b724b8e8b
-size 1272
+oid sha256:add90308f1c2a645b42158f8be9022163d6fcfd6ccd7906052a3c0b29c3c97e5
+size 1324
