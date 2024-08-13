11ea314462f9ad451f39ec86706e9720623dacdb
박형준 <issg2030@gmail.com>
2024-08-13 10:56:37 +0900

    fix: zustand import 수정 (#91)
    Former-commit-id: 28f648c1c690d50171eac86ffbec0987405220c4

--- frontend/src/stores/authStore.tsx
+++ frontend/src/stores/authStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:608b188979bbaff63bfbcf5d95bb83d28600eb70119cfeb9c256b1267bd5b288
-size 1999
+oid sha256:3479a8312ff399de39bf809db52ae0e2970bed84776dcf3cda123eb055a269a8
+size 2003
