45497d4bc229c6e0870f4f1ce3c5655d6e550292
박민준 <ppmm98@naver.com>
2024-08-13 15:16:45 +0900

    refactor: Reservation with Code
    Former-commit-id: 88d8397139f5897793199437a197e7555c0c58f5

---
+++ backend/src/main/java/com/mamdaero/domain/reservation/dto/response/SymptomResponse.java
@@ -0,0 +1,12 @@
+package com.mamdaero.domain.reservation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SymptomResponse {
+    private String symptomName;
+}
