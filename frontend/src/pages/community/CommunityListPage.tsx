64ee722686327341124047ce1b24b4b65340de2c
신혜민 <alddlv81@gmail.com>
2024-08-13 14:23:18 +0900

    feat: 게시판 검색 기능 추가 (#92)
    Former-commit-id: dc77d5a4e7bca08053ca402f4cb947ca52c13417

--- frontend/src/pages/community/CommunityListPage.tsx
+++ frontend/src/pages/community/CommunityListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c462e1d41b722636803c851b1df35c4a80f360cb575695c78f7836ab19e255e0
-size 3149
+oid sha256:0a975982b46266eacd1784d79862c4aff3caf035e3a0cc8ef16792b7346cd11d
+size 4030
