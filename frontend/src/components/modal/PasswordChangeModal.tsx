0d250a6bd8f5df33bb6b8a12d4670a687a281c5d
허세령 <seryoii@naver.com>
2024-08-14 03:10:22 +0900

    fix: 포스트잇 입력창 초기화되게 수정 (#71)
    Former-commit-id: 892c73831804d0963a3dd97d58e3f407e94e5daf

--- frontend/src/components/modal/PasswordChangeModal.tsx
+++ frontend/src/components/modal/PasswordChangeModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad5afdee9efc22ac452fc222aa426aa5e15a63ceb0cd49e848edc7e21e2b6627
-size 5288
+oid sha256:c95127d45ab80db432c3ec20e66f17fe975de96733c2f2604924bda71cd997fa
+size 5552
--- frontend/src/pages/mypage/ClientMyPage.tsx
+++ frontend/src/pages/mypage/ClientMyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cc756f5c9c3cc35dea1e8b6dd4e03492339b016f4d933d8c3ebdeffe3448af1
-size 9494
+oid sha256:7339acb5afb03fd17332eb6a0d70839415991a5ae43f254c7e03443199ea4754
+size 12001
