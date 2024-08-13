bd19f494b2d7764a4d95da85d150d2529a947b76
허세령 <seryoii@naver.com>
2024-08-14 03:58:59 +0900

    fix: 내담자 예약 취소 (#93)
    Former-commit-id: c832bef9eae460d04ea77867995874057c4d12a8

--- frontend/src/components/card/mycounsel/ClientReservationStatusCard.tsx
+++ frontend/src/components/card/mycounsel/ClientReservationStatusCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4444d1390cc48cf141353cdfe8e88b92a40bc744d37a2afc5e6362f36f416ef3
-size 5418
+oid sha256:1e4530e2a7fbc4ea982f7d0280b0acb440ccbaa3c5550311b5990f3c7e6af4a3
+size 5417
