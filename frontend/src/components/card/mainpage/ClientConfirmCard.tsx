63b90d2b4bf98dea34cd943e600756c53d2b9bc6
박주영 <pjy7244@gmail.com>
2024-08-15 00:45:04 +0900

    add: 내담자 메인 컨펌카드 이름 수정 (#100)
    Former-commit-id: 4534a4c50db6d6684cdb861addcf36d74af19712

--- frontend/src/components/card/mainpage/ClientConfirmCard.tsx
+++ frontend/src/components/card/mainpage/ClientConfirmCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c9a16524d1b676064a00d343fef153706a8a8ec107fe8b64c62bcc3b00acbfa
-size 2325
+oid sha256:db3b7ab09f097416f894f0b5497637b92edc9aeb6f65e87995f477d7aa3c8702
+size 2327
