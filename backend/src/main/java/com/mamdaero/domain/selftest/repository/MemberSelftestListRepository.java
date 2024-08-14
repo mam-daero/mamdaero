2d311f0db61d2e9d74384613881773a6f78e884a
박민준 <ppmm98@naver.com>
2024-08-14 10:59:20 +0900

    refactor: SelfTest result
    Former-commit-id: c08a2e0a08bdb59881f7b7e7f7b3aa51a4fbf070

--- backend/src/main/java/com/mamdaero/domain/selftest/repository/MemberSelftestListRepository.java
+++ backend/src/main/java/com/mamdaero/domain/selftest/repository/MemberSelftestListRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0616f20b13fa03f9380fecfd7ed18d45ab7dbcacb7cacffd1c90218b36d2523
-size 818
+oid sha256:e40b6235e77592207197728bac00803e01e160550510abe85f7c431dd2a91db9
+size 1001
