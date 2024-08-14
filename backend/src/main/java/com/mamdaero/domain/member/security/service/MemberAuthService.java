4b866fcc05f59b0280e164a9f2162935d3adb9ee
손동희 <riingfan04@gmail.com>
2024-08-14 14:08:49 +0900

    로그인 시 SecurityContextHolder 일부 수정
    Former-commit-id: c84316addcaff68d9d59cb9a2c8a5c29bac72008

--- backend/src/main/java/com/mamdaero/domain/member/security/service/MemberAuthService.java
+++ backend/src/main/java/com/mamdaero/domain/member/security/service/MemberAuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe355cb0b93be8dd874c016d01fe50d78bdbe54da144a33f5188b3efc931d23
-size 8390
+oid sha256:b24f9ac547014685bf8207e362ce6626bf689cc650807ca29bf40d6265fc9d3b
+size 8434
