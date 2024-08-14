0ee60012118860234de36ef7a5e9b9c4a215fc2c
허세령 <seryoii@naver.com>
2024-08-14 14:24:04 +0900

    fix: 상담사 마이 페이지 회원 탈퇴 추가 (#95)
    Former-commit-id: f978dec8808ba88798ea4f0aa41ae098aeee86ec

--- frontend/src/pages/mypage/CounselorMyPage.tsx
+++ frontend/src/pages/mypage/CounselorMyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bb9acf57d57776012a723b7f1f57cd753050a4d77a9a173be0ca63a12b5dd8b
-size 15587
+oid sha256:0931733c0065345c1019f973905b8c7f4bb9133568c7ef98216be03b2c6a10c5
+size 17411
