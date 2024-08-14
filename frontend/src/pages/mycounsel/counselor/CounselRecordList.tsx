eb840b927e8fe5e808f9a9195d5430843a82eaca
박주영 <pjy7244@gmail.com>
2024-08-14 11:49:52 +0900

    feat: 상태메세지 컴포넌트 (#77)
    Former-commit-id: c3d61d11323afedb2e35c618c0fc48941f7b697a

--- frontend/src/pages/mycounsel/counselor/CounselRecordList.tsx
+++ frontend/src/pages/mycounsel/counselor/CounselRecordList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce614d8fabba6a2b0114857210df974199961afcf76aa58bfd7d15244976db18
-size 4503
+oid sha256:f5b28b401e2cca7cf5223c68c6f4ef40cf6d02b05d4fd2319f177606e7bf6b18
+size 3100
