5f38a6efa302ed3dd1097d96b27e4e8be0c39b9c
허세령 <seryoii@naver.com>
2024-08-16 04:26:52 +0900

    fix: 챗봇 질문 폭격 수정
    Former-commit-id: 0d2e1d8f8c00f1b5a6406229f1844717e3ef3e7b

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14adb84083e05abda09b65536596ffea2e6721f77fee0e52021d9ce388b25754
-size 9135
+oid sha256:8e927824acfb5f05683791ae8261220a29c9d0081ccf24fbc88079be9fd650ce
+size 9689
