c8219c461a00d7750e64d9eddce62872ae1e90c1
허세령 <seryoii@naver.com>
2024-08-16 03:03:49 +0900

    fix: 소셜 로그인 버튼 삭제
    Former-commit-id: f31994a8a0f566c1baa98b9848e9078f7a7101e3

--- frontend/src/components/input/LoginClient.tsx
+++ frontend/src/components/input/LoginClient.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad4a00745aae8761dc7b88961fbe4632e86c10f696abdd4c4d23f7e5ec93037d
-size 4340
+oid sha256:c3a3fb76ca3c3306d8f62a373504d6477068dad4b4398d11c510cda5ae0f62bd
+size 4470
