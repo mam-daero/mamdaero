72abd139e2572b4fadadf58091f4d2e6ce5eab1e
박민준 <ppmm98@naver.com>
2024-08-16 00:46:32 +0900

    refactor: Counselor Access
    Former-commit-id: 51b217418553672c09bb246dc1b0391be51e979b

--- backend/src/main/java/com/mamdaero/domain/reservation/repository/ReservationRepository.java
+++ backend/src/main/java/com/mamdaero/domain/reservation/repository/ReservationRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15c93cd6def54287ee9e757dcc7f3eb29b8565aa26ea17cb4f4d500b8d8d58b6
-size 4862
+oid sha256:70e1830613d681b4d3859d50ce4f012b0eec96593383db91dca0533b99a27a26
+size 4949
--- backend/src/main/java/com/mamdaero/domain/reservation/service/ReservationService.java
+++ backend/src/main/java/com/mamdaero/domain/reservation/service/ReservationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b78a787d84dfeb4530d4ae4d9e46143645853297b7abe51fa4e15f95d13981b2
-size 11697
+oid sha256:7d39c43fbe14819882bdcd8084120f4a2c1a32e5dda7a74ad4fe4385122fe9d4
+size 11792
