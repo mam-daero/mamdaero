07e25c4663db27400552cb6421b51f8f4f040047
박형준 <issg2030@gmail.com>
2024-08-16 02:01:00 +0900

    fix: 페이지네이션 사이즈 수정
    Former-commit-id: 2448d52fbf02b2042924d0841c44a5a66e44363b

--- frontend/src/pages/counselor/CounselorListPage.tsx
+++ frontend/src/pages/counselor/CounselorListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f9dcd491b5fa880c4661d46bfbaccb35283db62100c8841ae262973fa511537
-size 4792
+oid sha256:23840949f2ee6b81687037eac1c4689f837bc678d342a7b98be5be1e73412692
+size 4830
--- frontend/src/pages/mycounsel/props/reservationApis.ts
+++ frontend/src/pages/mycounsel/props/reservationApis.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7f3089b127af7a3f5a7836ee3ea2f912c96676eb7171324854465f30fdb7de3
-size 1289
+oid sha256:c455a502bb3602fade1f84966e2f65e7411c88d82c0f8bf4b2f137a7ee353465
+size 1365
