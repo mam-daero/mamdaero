1ffa36706e337b1bb3664f314d7dc173bd53d7b7
박형준 <issg2030@gmail.com>
2024-08-15 23:33:40 +0900

    fix: 상대방 대기 문구 수정
    Former-commit-id: 50e08571780863dd96699714ba96691baafb5817

--- frontend/src/pages/mycounsel/counselor/CounselorFaceChat2.tsx
+++ frontend/src/pages/mycounsel/counselor/CounselorFaceChat2.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c669e40fe98d435d69cb1cf76f7c70478b53f3cc1073ff460fbcbfbb3e39151
+oid sha256:78f8c6a0952f16b43a28a8d87e5f441d63e2efda167687bc31feb5ad22f6a1eb
 size 18856
